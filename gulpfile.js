16d3b6ae3dec9345cd8aeda3d6aff39b95a85548
migitera takanobu <t.EDEN.m@gmail.com>
2016-04-30 16:21:42 +0900

    update package

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,53 @@
 'use strict';
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var plumber = require('gulp-plumber');
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+
+var data = require('gulp-data');
+var plumber = require('gulp-plumber');
+
 var sass = require('gulp-sass');
 var pleeease = require('gulp-pleeease');
-var header = require('gulp-header');
 var csscomb = require('gulp-csscomb');
 var cssmin = require('gulp-cssmin');
+
 var jade = require('gulp-jade');
+
+var sitemap = require('gulp-sitemap-files');
+
+var del = require('del');
+var runSequence = require('run-sequence');
+var header = require('gulp-header');
+
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var del = require('del');
-var runSequence = require('run-sequence');
+
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+var svgmin = require('gulp-svgmin');
+var iconfontCss = require('gulp-iconfont-css');
 
 var AUTOPREFIXER_BROWSERS = [
   'ie >= 10',
   'ff >= 30',
-  'chrome >= 39',
+  'chrome >= 34',
   'safari >= 7',
   'opera >= 23',
 ];
-
-gulp.task('sass', function() {
-  return gulp.src(['./src/scss/*.scss', './src/scss/**/_*.scss'])
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(pleeease({
-      minifier: false,
-      autoprefixer: AUTOPREFIXER_BROWSERS
-    }))
-    .on('error', console.error.bind(console))
-    .pipe(header('@charset "utf-8";\n'))
-    .pipe(gulp.dest('./dist/'))
-    .on('end', reload);
-});
-
-gulp.task('sass:deproy', function() {
-  return gulp.src(['src/scss/*.scss', 'src/scss/**/_*.scss'])
-    .pipe(sass())
-    .pipe(pleeease({
-      minifier: false,
-      autoprefixer: AUTOPREFIXER_BROWSERS
-    }))
-    .pipe(csscomb())
-    .pipe(cssmin())
-    .pipe(header('@charset "utf-8";\n'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('jade', function() {
-  return gulp.src(['./src/jade/*.jade', 'src/jade/**/*.jade', '!src/jade/**/_*.jade'])
-    .pipe(plumber())
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .on('end', reload);
-});
-
-gulp.task('coffee', function() {
-  return gulp.src(['./src/coffee/*.coffee', './src/coffee/**/_*.coffee'])
-    .pipe(plumber())
-    .pipe(coffee({
-      bare: true
-    }))
-    .on('error', console.error.bind(console))
-    .pipe(gulp.dest('./dist/js/'))
-});
-
-gulp.task('compile-js', function() {
-  var compileFileName = 'application.js'
-  return gulp.src(['./src/js/*.js', '!src/js/' + compileFileName])
-    .pipe(uglify())
-    .pipe(concat(compileFileName))
-    .pipe(gulp.dest('./dist/assets/js/'))
-    .on('end', reload);
-});
-
-
-gulp.task('image', function() {
-  return gulp.src(['./src/img/**/*', '/src/img/*'])
-    .pipe(gulp.dest('./dist/img/'));
-});
-
-gulp.task('clean', function(callback) {
-  del(['./dist/'], callback);
-});
-
-gulp.task('build', function(callback) {
-  //  runSequence('clean', ['jade', 'sass:deproy', 'coffee', 'compile-js', 'image'], callback);
-  runSequence(['jade', 'sass:deproy', 'coffee', 'compile-js', 'image'], callback);
-});
+var pkg = require('./package.json');
+var BANNER = [
+  '@charset "utf-8";',
+  '/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @link <%= pkg.url %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @Author <%= pkg.author %>',
+  ' * @Author URI <%= pkg.author %>',
+  ' */',
+  ''
+].join('\n');
 
 gulp.task('default', function() {
   browserSync({
@@ -108,3 +62,118 @@ gulp.task('default', function() {
   gulp.watch(['./src/coffee/*.coffee', './src/coffee/**/_*.coffee'], ['coffee']);
   gulp.watch(['./src/js/*.js'], ['compile-js']);
 });
+
+
+gulp.task('sass', function() {
+  gulp.src(['src/scss/*.scss', 'src/scss/**/_*.scss'])
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(pleeease({
+      minifier: false,
+      autoprefixer: {
+        browsers: ['last 4 versions', 'Android 2.3']
+      },
+      //      autoprefixer: AUTOPREFIXER_BROWSERS,
+      //      autoprefixer: 'chrome >= 39'
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(header('@charset "utf-8";\n'))
+    .pipe(gulp.dest('./dist/css/'))
+    .on('end', reload);
+});
+
+gulp.task('sass:deproy', function() {
+  gulp.src(['src/scss/*.scss', 'src/scss/**/_*.scss'])
+    .pipe(sass())
+    .pipe(pleeease({
+      autoprefixer: {
+        browsers: ['last 4 versions', 'Android 2.3']
+      },
+      minifier: false
+    }))
+    .pipe(csscomb())
+    .pipe(cssmin())
+    .pipe(header(BANNER, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('jade', function() {
+  gulp.src(['./src/jade/*.jade', 'src/jade/**/*.jade', '!src/jade/**/_*.jade'])
+    .pipe(plumber())
+    //    .pipe(data(function(file) {
+    //      return require('./data.json');
+    //    }))
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist/'))
+    .on('end', reload);
+});
+
+gulp.task('coffee', function() {
+  gulp.src(['./src/coffee/*.coffee', './src/coffee/**/_*.coffee'])
+    .pipe(plumber())
+    .pipe(coffee({
+      bare: true
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(gulp.dest('./dist/js/'))
+});
+
+gulp.task('compile-js', function() {
+  var compileFileName = 'application.js'
+  gulp.src(['./src/js/*.js', '!src/js/' + compileFileName])
+    .pipe(uglify({preserveComments: 'some'}))
+    .pipe(concat(compileFileName))
+    .pipe(gulp.dest('./dist/js/'))
+    .on('end', reload);
+});
+
+gulp.task('sitemap', function() {
+  gulp.src('./dist/**/*.html')
+    .pipe(sitemap({
+      siteUrl: 'http://www.solana.asia/'
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('clean', function(cb) {
+  del(['./dist/**/*.html', './dist/**/*.css', './dist/**/*.xml', , './dist/**/*.js'], cb);
+});
+
+gulp.task('build', [], function(cb) {
+  runSequence(
+    ['sass:deproy', 'jade', 'coffee', 'compile-js'],
+    //    'sitemap',
+    cb
+  );
+});
+
+gulp.task('svgmin', function() {
+  gulp.src(['./src/icon/*.svg'])
+    .pipe(svgmin())
+    .pipe(gulp.dest('src/icon'));
+});
+
+gulp.task('makefont', function() {
+  gulp.src(['./src/icon/*.svg'])
+    .pipe(iconfontCss({
+      fontName: 'icon',
+      path: './src/scss/base/template/icon.scss',
+      targetPath: '../scss/base/_icon.scss',
+      fontPath: '/fonts/'
+    }))
+    .pipe(iconfont({
+      fontName: 'icon'
+    }))
+    .pipe(gulp.dest('./src/fonts/'))
+});
+
+gulp.task('moveicon', function() {
+  gulp.src('./src/fonts/*')
+    .pipe(gulp.dest('./dist/fonts/'));
+});
+
+gulp.task('iconfont', ['makefont', 'moveicon']);
